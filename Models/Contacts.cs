7e0ccbe1d1c43076d84c6659f4ce5380e3ff6a17
Francisco Vinueza <vinueza@yandex.ru>
2020-10-23 14:50:22 +0300

    Version 1

---
+++ Models/Contacts.cs
@@ -0,0 +1,13 @@
+namespace ApiSQLVin.Models
+{
+    public class Contacts
+    {
+        public string id {get ; set;}
+
+        public string Name {get; set;}
+
+        public string email {get; set}
+
+        public string phone { get; set; }
+    }
+}
